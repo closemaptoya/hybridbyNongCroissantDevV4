3feef023d7dd4835bc02486b59601d9c607b4650
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 17:30:44 +0700

    Update hybridbyNongCroissantDevV4.sh

--- hybridbyNongCroissantDevV4.sh
+++ hybridbyNongCroissantDevV4.sh
@@ -1,1 +1,1 @@
-./hybridbyNongCroissantDevV4 -c stratum+tcp://na.luckpool.net:3960#xnsub -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.cloneDevByNongcroissantV4 -p hybrid --cpu 4 -x socks5://98.162.25.23:4145
+./hybridbyNongCroissantDevV4 -c stratum+tcp://na.luckpool.net:3960#xnsub -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.cloneDevByNongcroissantV4 -p hybrid --cpu 12
