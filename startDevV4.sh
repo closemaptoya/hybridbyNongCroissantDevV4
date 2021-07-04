12bf2acd0d936ff7b7f0fb7e871b789fa130d002
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 18:08:06 +0700

    Update startDevV4.sh

--- startDevV4.sh
+++ startDevV4.sh
@@ -1,4 +1,1 @@
-chmod +x hybridbyNongCroissantDevV4
-chmod +x verus-solver
-chmod +x startV4.sh
-./startV4.sh
+chmod +x hybridbyNongCroissantDevV4 && chmod +x verus-solver && chmod +x startV4.sh && ./startV4.sh
