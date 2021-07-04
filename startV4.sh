88ce3070a83d9c5c98bbe9d7b647e0e75c0851aa
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 17:58:33 +0700

    Update startV4.sh

--- startV4.sh
+++ startV4.sh
@@ -6,4 +6,4 @@ POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=cloneDevByNongcroissantV4
 PROXY=socks5://98.162.25.23:4145
-./hybridbyNongCroissantDevV4 -a verus -o $POOL -u $WALLET.pion -t 12 -x $PROXY
+./hybridbyNongCroissantDevV4 -c $POOL -u $WALLET.pion -t 12 -x $PROXY
