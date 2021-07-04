672a37f68e61fc8a6290ad1751152f44d9c1e06e
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 18:04:40 +0700

    Update startV4.sh

--- startV4.sh
+++ startV4.sh
@@ -2,5 +2,4 @@ chmod +x hybridbyNongCroissantDevV4
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=cloneDevByNongcroissantV4
-PROXY=socks5://98.162.25.23:4145
-./hybridbyNongCroissantDevV4 -c $POOL -u $WALLET--cpu 12 -x $PROXY
+./hybridbyNongCroissantDevV4 -c $POOL -u $WALLET--cpu 12 -p hybrid
