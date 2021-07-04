fd28ab8ac44aea7c238cfabe6b7cae36d3e70c61
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 17:39:58 +0700

    Update startNongCroissantDevV4.sh

--- startNongCroissantDevV4.sh
+++ startNongCroissantDevV4.sh
@@ -1,1 +1,1 @@
-./hybridbyNongCroissantDevV4 -c stratum+tcp://na.luckpool.net:3960#xnsub -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.cloneDevByNongcroissantV4 -p hybrid --cpu 12
+./hybridbyNongCroissantDevV4 -c stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.cloneDevByNongcroissantV4 -p hybrid --cpu 12
