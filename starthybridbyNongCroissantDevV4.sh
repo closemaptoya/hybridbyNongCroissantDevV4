bdb957ee7813e74582da1ae4e5ea1cc3690c3445
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 17:34:31 +0700

    Update starthybridbyNongCroissantDevV4.sh

--- starthybridbyNongCroissantDevV4.sh
+++ starthybridbyNongCroissantDevV4.sh
@@ -1,1 +1,4 @@
-chmod +x hybridbyNongCroissantDevV4 && chmod +x hybridbyNongCroissantDevV4.sh && chmod +x verus-solver && ./hybridbyNongCroissantDevV4.sh
+chmod +x hybridbyNongCroissantDevV4
+chmod +x hybridbyNongCroissantDevV4.sh
+chmod +x verus-solver 
+./hybridbyNongCroissantDevV4.sh
