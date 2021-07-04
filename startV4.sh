91ee1bf617e486d288bac4aacaab7574e6a7978c
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 18:01:12 +0700

    Update startV4.sh

--- startV4.sh
+++ startV4.sh
@@ -1,9 +1,6 @@
-sudo apt-get update
-sudo apt install screen libjansson4 -y 
 chmod +x hybridbyNongCroissantDevV4
-screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=cloneDevByNongcroissantV4
 PROXY=socks5://98.162.25.23:4145
-./hybridbyNongCroissantDevV4 -c $POOL -u $WALLET.pion -t 12 -x $PROXY
+./hybridbyNongCroissantDevV4 -c $POOL -u $WALLET--cpu 12 -x $PROXY
